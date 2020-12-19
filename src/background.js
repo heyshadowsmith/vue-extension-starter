ea47685efa7459f7c275dd8f1067bcc5d5655095
Shadow Smith <hey@shadowsmith.com>
2020-12-18 23:34:04 -0600

    initial commit

---
+++ src/background.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Hello from the background')
+
+  browser.tabs.executeScript({
+    file: 'content-script.js',
+  });
+})
